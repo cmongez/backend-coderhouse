881d465b04d5f590461cdda2539fba33f82218cb
Cesar <camongez28@gmail.com>
2023-02-11 02:02:53 -0300

    finalizado endpoints de products

---
+++ primera-pre-entrega/src/app.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import productsRouter from './routes/products.router.js';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/products', productsRouter);
+
+app.listen(8080, () => console.log('Listening on 8080'));
