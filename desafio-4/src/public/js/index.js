8bd4eb3ae8bb79143f61e84bfe4e373a8a64d86f
Cesar <camongez28@gmail.com>
2023-02-26 16:29:33 -0300

    desafio-4

--- desafio-4/src/public/js/index.js
+++ desafio-4/src/public/js/index.js
@@ -1,24 +1,63 @@
 const socket = io();
 
-const form = document.getElementById('form');
+socket.on('message', console.log('Cliente conectado'));
 
-const container = document.getElementById('container');
+//------------------------------------------------------------------------------------
 
-socket.on('showProducts', (data) => {
-  container.innerHTML = ``;
+const formAddProduct = document.getElementById('formAddProduct');
+formAddProduct.addEventListener('submit', (e) => {
+  e.preventDefault();
 
-  data.forEach((prod) => {
-    container.innerHTML += `
-            <ul>
-                <li>title: ${prod.title}</li> 
-                <li>description: ${prod.description}</li>
-                <li>code: ${prod.code}</li>
-                <li>price: ${prod.price}</li>
-                <li>status: ${prod.status}</li>
-                <li>stock: ${prod.stock}</li>
-                <li>category: ${prod.category}</li>
-                <li>id: ${prod.id}</li>
-            </ul>
-        `;
+  const productToAdd = {
+    title: document.getElementById('titulo').value,
+    description: document.getElementById('descripcion').value,
+    price: document.getElementById('precio').value,
+    thumbnail: document.getElementById('imagen').value,
+    code: document.getElementById('code').value,
+    stock: document.getElementById('stock').value,
+    status: document.getElementById('status').value,
+  };
+  socket.emit('addProduct', productToAdd);
+
+  socket.on('productAdded', (product) => {
+    console.log('El producto añadido es', product);
+
+    const productToRender = {
+      id: product.id,
+      title: product.title,
+      description: product.description,
+      price: product.price,
+      thumbnail: product.thumbnail,
+      code: product.code,
+      stock: product.stock,
+      status: product.status,
+    };
+
+    const productTable = document.getElementById('product_table');
+    const productTemplate = `<tr>
+        <th scope="row" >{{id}}</th>
+        <th >{{title}}</th>
+        <th >{{description}}</th>
+        <th ><img class="imagen" src="{{thumbnail}}" alt=""></th>
+        <th >{{code}}</th>
+        <th >{{stock}}</th>
+        <th >{{status}}</th>
+        <th >{{price}}</th>
+        </tr>`;
+    const template = Handlebars.compile(productTemplate);
+    const productHTML = template(productToRender);
+    productTable.innerHTML += productHTML;
   });
+});
+
+const deleteProduct = (id) => {
+  console.log('Producto a eliminar :>> ', id);
+  socket.emit('deleteProductById', id);
+};
+
+socket.on('productDeleted', (id) => {
+  console.log('Producto eliminado :>> ', id);
+  let products = document.getElementById('product_table');
+  let productToDelet = document.getElementById(id);
+  let productDeleted = products.removeChild(productToDelet);
 });
