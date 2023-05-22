a558324e546f8f70f58cbb5d541956a0e28c4851
cmongez <camongez28@gmail.com>
2023-05-21 21:15:28 -0400

    Desafio login por formulario finalizado. Clase 19.

--- desafio-5_c_20/src/public/js/main.js
+++ desafio-5_c_20/src/public/js/main.js
@@ -1,16 +1,15 @@
 // Espera a que la página se cargue completamente
-document.addEventListener('DOMContentLoaded', async function() {
+document.addEventListener('DOMContentLoaded', async function () {
   // Obtener el valor de cartId desde el localStorage
   const cartId = localStorage.getItem('cartId');
-  console.log("HOLA",cartId)
-  
+
   const cartButton = document.querySelector('#cartButton');
-  
+
   // Si cartId existe, actualiza el href del botón del carrito
   if (cartId) {
     const href = `/carts/${cartId}`;
     cartButton.href = href;
-  }else{
+  } else {
     try {
       const response = await fetch('/api/carts', {
         method: 'POST',
@@ -19,24 +18,18 @@ document.addEventListener('DOMContentLoaded', async function() {
         },
       });
       const data = await response.json();
-      console.log("Retorno creacion carro primera vez", data)
+      console.log('Retorno creacion carro primera vez', data);
       localStorage.setItem('cartId', data._id);
 
-    const href = `/carts/${data._id}`;
-    cartButton.href = href;
-
-
+      const href = `/carts/${data._id}`;
+      cartButton.href = href;
     } catch (error) {
       console.error('Error adding product to cart:', error);
     }
   }
-
-
-
-  
 });
 
-async function addProductToCart  (cartId,productId){
+async function addProductToCart(cartId, productId) {
   const url = `/api/carts/${cartId}/products/${productId}`;
   try {
     const response = await fetch(url, {
@@ -47,44 +40,40 @@ async function addProductToCart  (cartId,productId){
     });
     const data = await response.json();
     console.log('Product added to cart:', data);
-    return data
+    return data;
   } catch (error) {
     console.error('Error adding product to cart:', error);
   }
-
 }
 
 async function buttonAdd(button) {
   //Obtenemos el id del producto
   const productId = button.getAttribute('pid');
-  console.log(productId)
   //Verificamos si ya existe un cartId
   const cartId = localStorage.getItem('cartId');
 
-  
-    //Traemos el carrito
-    const cart = await fetch(`/api/carts/${cartId}`).then((res) => res.json());
-    console.log("CART",cart)
-    //Verificamos si el producto ya existe en el carrito
-    const existingProduct = cart.products.find((p) => p.product._id === productId);
-    console.log("existing",existingProduct)
+  //Traemos el carrito
+  const cart = await fetch(`/api/carts/${cartId}`).then((res) => res.json());
+  //Verificamos si el producto ya existe en el carrito
+  const existingProduct = cart.products.find(
+    (p) => p.product._id === productId
+  );
+  console.log('existing', existingProduct);
 
-    if (existingProduct) {
-      const newQuantity = existingProduct.quantity + 1;
-      
-      await fetch(`/api/carts/${cartId}/products/${productId}`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ quantity: newQuantity }),
-      });
-    } else {
-      addProductToCart(cartId, productId);
-    }
- 
+  if (existingProduct) {
+    const newQuantity = existingProduct.quantity + 1;
+
+    await fetch(`/api/carts/${cartId}/products/${productId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ quantity: newQuantity }),
+    });
+  } else {
+    addProductToCart(cartId, productId);
+  }
 }
-
 
 async function nextPage() {
   let params = new URLSearchParams(window.location.search);
--- desafio-5_c_20/src/routes/web/views.router.js
+++ desafio-5_c_20/src/routes/web/views.router.js
@@ -1,5 +1,4 @@
 import { Router } from 'express';
-// import ProductManager from '../daos/fileManager/ProductManager.js';
 import ProductManager from '../../daos/dbManager/products.dao.js';
 import CartsManager from '../../daos/dbManager/carts.dao.js';
 
@@ -7,7 +6,33 @@ const router = Router();
 
 const productManager = new ProductManager();
 const cartsManager = new CartsManager();
-router.get('/products', async (req, res) => {
+
+const publicAccess = (req, res, next) => {
+  if (req.session.user) return res.redirect('/products');
+  next();
+};
+
+const privateAccess = (req, res, next) => {
+  if (!req.session.user) return res.redirect('/login');
+  next();
+};
+
+router.get('/register', publicAccess, (req, res) => {
+  res.render('register');
+});
+
+router.get('/login', publicAccess, (req, res) => {
+  res.render('login');
+});
+
+router.get('/', privateAccess, (req, res) => {
+  res.render('profile', {
+    user: req.session.user,
+  });
+});
+
+//Products and Carts
+router.get('/products', privateAccess, async (req, res) => {
   const { limit = 10, page = 1, sort, category, status } = req.query;
 
   const filter = {};
@@ -25,8 +50,11 @@ router.get('/products', async (req, res) => {
       limit,
       page
     );
-    console.log(products);
-    res.render('products', products);
+
+    res.render('products', {
+      products,
+      user: req.session.user,
+    });
   } catch (error) {
     res.status(500).json({ status: 'error', message: error.message });
   }
@@ -47,18 +75,3 @@ router.get('/carts/:cid', async (req, res) => {
 });
 
 export default router;
-
-// router.get('', async (req, res) => {
-//   const products = await productManager.getProducts();
-//   res.render('home', { products });
-// });
-
-// router.get('/realtimeproducts', async (req, res) => {
-//   console.log('entre');
-//   const products = await productManager.getProducts();
-//   res.render('realTimeProducts', { products });
-// });
-
-// router.get('/chat', (req, res) => {
-//   res.render('chat');
-// });
